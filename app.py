5ab79c3818ed1945dafe4058b1a8b19f5dcad9b9
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2025-10-29 09:03:53 -0600

    Use session_state to manage count variable
    Refactor code to use session_state for count management.

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 import streamlit as st
 st.title("Ejemplo para usar session_state")
 
-count = 0
+if 'count' not in st.session_state:
+    st.session_state['count'] = 0
 
-increment = st.button('Increment')
-if increment:
-    count += 1
-
-st.write('Count = ', count)
 
 st.write(st.session_state)
